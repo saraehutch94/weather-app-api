1b0d04a4b062fc75a033e5cdc26f8e514e44ec1e
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-24 14:05:20 -0400

    wip: connect form submission to ajax call, added ajax call to handleSubmit function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const $temp = $("#temp");
 const $feelsLike = $("#feels-like");
 const $weather = $("#weather");
 const $form = $("form");
+const $input = $("input[type='text']");
 
 // Event Listeners
 
@@ -18,8 +19,26 @@ $form.on("submit", handleSubmit);
 
 // Functions
 
-function handleSubmit() {
-    console.log("form submitted!");
+function handleSubmit(evt) {
+    //console.log("form submitted!");
+    evt.preventDefault();
+    const cityName = $input.val();
+    console.log(cityName);
+
+    $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700&units=imperial")
+    .then(function(data) {
+        // success callback function
+        // console.log("promise fulfilled!");
+        // console.log(data);
+        $city.text(data.name);
+        $temp.text(data.main.temp);
+        $feelsLike.text(data.main.feels_like);
+        $weather.text(data.weather[0].description);
+    }, function(error) {
+        // failure callback function
+        console.log("promise failed");
+        console.log(error);
+    });
 };
 
 // const promise = $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700");
