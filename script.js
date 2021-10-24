a6960008b840abf18844e445e865b55a508f6f50
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-24 13:46:17 -0400

    add then method to promise

--- script.js
+++ script.js
@@ -12,6 +12,17 @@
 
 // Functions
 
-const promise = $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700");
+// const promise = $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700");
 
-console.log(promise);
+// console.log(promise);
+
+$.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700")
+.then(function(data) {
+    // success callback function
+    console.log("promise fulfilled!");
+    console.log(data);
+}, function(error) {
+    // failure callback function
+    console.log("promise failed");
+    console.log(error);
+});
