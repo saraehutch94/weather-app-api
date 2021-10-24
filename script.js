da619737c095fef9421bb18fd8caa56a05c2cd1a
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-24 13:58:42 -0400

    added form to html

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ const $weather = $("#weather");
 
 // console.log(promise);
 
-$.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700")
+$.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700&units=imperial")
 .then(function(data) {
     // success callback function
     // console.log("promise fulfilled!");
-    console.log(data);
+    // console.log(data);
     $city.text(data.name);
     $temp.text(data.main.temp);
     $feelsLike.text(data.main.feels_like);
