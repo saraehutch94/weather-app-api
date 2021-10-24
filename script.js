6a1f237f5cbf35b6dce09f364b059d4381ac74cc
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-24 13:44:08 -0400

    updated AJAX method and JS promise

--- script.js
+++ script.js
@@ -12,7 +12,6 @@
 
 // Functions
 
-
-const promise = $.ajax("https://openweathermap.org/data/2.5/weather?q=Boston$appid=636c081e5a0b64f047edce5f8e863700");
+const promise = $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=636c081e5a0b64f047edce5f8e863700");
 
 console.log(promise);
