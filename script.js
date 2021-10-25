53824683936c39de0eb992494991851f1b8c4a20
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-25 12:06:13 -0400

    wip: organized weather days

--- script.js
+++ script.js
@@ -41,88 +41,127 @@ function handleSubmit(evt) {
 function render() {
     if ($select.val() === "1") {
         $main.html(`
-        <h3>Weather for</h3>
-        <p id="city">${cityData.city.name}</p>
+        <h2>${cityData.city.name} Weather</h2>
         <h4>Temperature</h4>
         <p id="temp">${cityData.list[4].main.temp}&#8457;</p>
-        <h4>Feels like</h4>
+        <h4>Feels Like</h4>
         <p id="feels-like">${cityData.list[4].main.feels_like}&#8457;</p>
-        <h4>Weather</h4>
+        <h4>Details</h4>
         <p id="weather">${cityData.list[4].weather[0].description}</p>
         `);
     } else if ($select.val() === "2") {
         $main.html(`
-        <h3 class="weather-for">Weather for</h3>
-        <p id="city">${cityData.city.name}</p>
+        <h2 class="weather-for">${cityData.city.name} Weather</h2>
+        <h3>Today</h3>
         <h4>Temperature</h4>
-        <p id="temp">Today ${cityData.list[4].main.temp}&#8457;</p>
-        <p id="temp-2nd-day">Tomorrow ${cityData.list[12].main.temp}&#8457;</p>
-        <h4>Feels like</h4>
-        <p id="feels-like">Today ${cityData.list[4].main.feels_like}&#8457;</p>
-        <p id="feels-like-2nd-day">Tomorrow ${cityData.list[12].main.feels_like}&#8457;</p>
+        <p id="temp">${cityData.list[4].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like">${cityData.list[4].main.feels_like}&#8457;</p>
         <h4>Weather</h4>
-        <p id="weather">Today ${cityData.list[4].weather[0].description}</p>
-        <p id="weather-2nd-day">Tomorrow ${cityData.list[12].weather[0].description}</p>
+        <p id="weather">${cityData.list[4].weather[0].description}</p>
+        <h3>Tomorrow</h3>
+        <h4>Temperature</h4>
+        <p id="temp-2nd-day">${cityData.list[12].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-2nd-day">${cityData.list[12].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-2nd-day">${cityData.list[12].weather[0].description}</p>
         `);
     } else if ($select.val() === "3") {
         $main.html(`
-        <h3 class="weather-for">Weather for:</h3>
-        <p id="city">${cityData.city.name}</p>
+        <h2 class="weather-for">${cityData.city.name} Weather</h3>
+        <h3>Today</h3>
         <h4>Temperature</h4>
-        <p id="temp">Today ${cityData.list[4].main.temp}&#8457;</p>
-        <p id="temp-2nd-day">Tomorrow ${cityData.list[12].main.temp}&#8457;</p>
-        <p id-"temp-3rd-day">${day3} ${cityData.list[20].main.temp}&#8457;</p>
-        <h4>Feels like</h4>
-        <p id="feels-like">Today ${cityData.list[4].main.feels_like}&#8457;</p>
-        <p id="feels-like-2nd-day">Tomorrow ${cityData.list[12].main.feels_like}&#8457;</p>
-        <p id="feels-like-3rd-day">${day3} ${cityData.list[20].main.feels_like}&#8457;</p>
+        <p id="temp">${cityData.list[4].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like">${cityData.list[4].main.feels_like}&#8457;</p>
         <h4>Weather</h4>
-        <p id="weather">Today ${cityData.list[4].weather[0].description}</p>
-        <p id="weather-2nd-day">Tomorrow ${cityData.list[12].weather[0].description}</p>
-        <p id="weather-3rd-day">${day3} ${cityData.list[20].weather[0].description}</p>
+        <p id="weather">${cityData.list[4].weather[0].description}</p>
+        <h3>Tomorrow</h3>
+        <h4>Temperature</h4>
+        <p id="temp-2nd-day">${cityData.list[12].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-2nd-day">${cityData.list[12].main.feels_like}&#8457;</p>
+        <h4>Weather</h4>
+        <p id="weather-2nd-day">${cityData.list[12].weather[0].description}</p>
+        <h3>${day3}</h3>
+        <h4>Temperature</h4>
+        <p id-"temp-3rd-day">${cityData.list[20].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-3rd-day">${cityData.list[20].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-3rd-day">${cityData.list[20].weather[0].description}</p>
         `);
     } else if ($select.val() === "4") {
         $main.html(`
-        <h3 class="weather-for">Weather for</h3>
-        <p id="city">${cityData.city.name}</p>
+        <h2 class="weather-for">${cityData.city.name} Weather</h2>
+        <h3>Today</h3>
         <h4>Temperature</h4>
-        <p id="temp">Today ${cityData.list[4].main.temp}&#8457;</p>
-        <p id="temp-2nd-day">Tomorrow ${cityData.list[12].main.temp}&#8457;</p>
-        <p id="temp-3rd-day">${day3} ${cityData.list[20].main.temp}&#8457;</p>
-        <p id="temp-4th-day">${day4} ${cityData.list[28].main.temp}&#8457;</p>
+        <p id="temp">${cityData.list[4].main.temp}&#8457;</p>
         <h4>Feels like</h4>
-        <p id="feels-like">Today ${cityData.list[4].main.feels_like}&#8457;</p>
-        <p id="feels-like-2nd-day">Tomorrow ${cityData.list[12].main.feels_like}&#8457;</p>
-        <p id="feels-like-3rd-day">${day3} ${cityData.list[20].main.feels_like}&#8457;</p>
-        <p id="feels-like-4th-day">${day4} ${cityData.list[28].main.feels_like}&#8457;</p>
-        <h4>Weather</h4>
-        <p id="weather">Today ${cityData.list[4].weather[0].description}</p>
-        <p id="weather-2nd-day">Tomorrow ${cityData.list[12].weather[0].description}</p>
-        <p id="weather-3rd-day">${day3} ${cityData.list[20].weather[0].description}</p>
-        <p id="weather-4th-day">${day4} ${cityData.list[28].weather[0].description}</p>
+        <p id="feels-like">${cityData.list[4].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather">${cityData.list[4].weather[0].description}</p>
+        <h3>Tomorrow</h3>
+        <h4>Temperature</h4>
+        <p id="temp-2nd-day">${cityData.list[12].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-2nd-day">${cityData.list[12].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-2nd-day">${cityData.list[12].weather[0].description}</p>
+        <h3>${day3}</h3>
+        <h4>Temperature</h4>
+        <p id="temp-3rd-day">${cityData.list[20].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-3rd-day">${cityData.list[20].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-3rd-day">${cityData.list[20].weather[0].description}</p>
+        <h3>${day4}</h3>
+        <h4>Temperature</h4>
+        <p id="temp-4th-day">${cityData.list[28].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-4th-day">${cityData.list[28].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-4th-day">${cityData.list[28].weather[0].description}</p>
         `);
     } else {
         $main.html(`
-        <h3 class="weather-for">Weather for:</h3>
-        <p id="city">${cityData.city.name}</p>
+        <h2 class="weather-for">${cityData.city.name} Weather</h3>
+        <h3>Today</h3>
         <h4>Temperature</h4>
-        <p id="temp">Today ${cityData.list[4].main.temp}&#8457;</p>
-        <p id="temp-2nd-day">Tomorrow ${cityData.list[12].main.temp}&#8457;</p>
-        <p id="temp-3rd-day">${day3} ${cityData.list[20].main.temp}&#8457;</p>
-        <p id="temp-4th-day">${day4} ${cityData.list[28].main.temp}&#8457;</p>
-        <p id="temp-5th-day">${day5} ${cityData.list[36].main.temp}&#8457;</p>
-        <h4>Feels like</h4>
-        <p id="feels-like">Today ${cityData.list[4].main.feels_like}&#8457;</p>
-        <p id="feels-like-2nd-day">Tomorrow ${cityData.list[12].main.feels_like}&#8457;</p>
-        <p id="feels-like-3rd-day">${day3} ${cityData.list[20].main.feels_like}&#8457;</p>
-        <p id="feels-like-4th-day">${day4} ${cityData.list[28].main.feels_like}&#8457;</p>
-        <p id="feels-like-5th-day">${day5} ${cityData.list[36].main.feels_like}&#8457;</p>
-        <h4>Weather:</h4>
-        <p id="weather">Today ${cityData.list[4].weather[0].description}</p>
-        <p id="weather-2nd-day">Tomorrow ${cityData.list[12].weather[0].description}</p>
-        <p id="weather-3rd-day">${day3} ${cityData.list[20].weather[0].description}</p>
-        <p id="weather-4th-day">${day4} ${cityData.list[28].weather[0].description}</p>
-        <p id="weather-5th-day">${day5} ${cityData.list[36].weather[0].description}</p>
+        <p id="temp">${cityData.list[4].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like">${cityData.list[4].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather">${cityData.list[4].weather[0].description}</p>
+        <h3>Tomorrow</h3>
+        <h4>Temperature</h4>
+        <p id="temp-2nd-day">${cityData.list[12].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-2nd-day">${cityData.list[12].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-2nd-day">${cityData.list[12].weather[0].description}</p>
+        <h3>${day3}</h3>
+        <h4>Temperature</h4>
+        <p id="temp-3rd-day">${cityData.list[20].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-3rd-day">${cityData.list[20].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-3rd-day">${cityData.list[20].weather[0].description}</p>
+        <h3>${day4}</h3>
+        <h4>Temperature</h4>
+        <p id="temp-4th-day">${cityData.list[28].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-4th-day">${cityData.list[28].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-4th-day">${cityData.list[28].weather[0].description}</p>
+        <h3>${day5}</h5>
+        <h4>Temperature</h4>
+        <p id="temp-5th-day">${cityData.list[36].main.temp}&#8457;</p>
+        <h4>Feels Like</h4>
+        <p id="feels-like-5th-day">${cityData.list[36].main.feels_like}&#8457;</p>
+        <h4>Details</h4>
+        <p id="weather-5th-day">${cityData.list[36].weather[0].description}</p>
         `);
     }
 };
